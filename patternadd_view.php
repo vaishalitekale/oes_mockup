12deeddb0bdd33f2e8c381f29e0b517a72539918
vaishalitekale <vaishali.tekale@gmail.com>
2013-06-12 17:48:22 +0530

    Update patternadd_view.php

--- patternadd_view.php
+++ patternadd_view.php
@@ -9,13 +9,14 @@
 			
             <input type="hidden" id="subject" name="subject" value="<?php echo $subjectid; ?>" />
 				<fieldset style="margin-top: 40px;">
+				
                 <table >
                         <th >
                         <td>Chapter Name</td>
                         <td>Pattern1</td>
                         <td>Pattern2</td>
                         <td>Pattern3</td>
-						<td>Available Question</td>
+                        <td>Available Question</td>
                         </th>
                 <?php
                 $i=1; 
@@ -30,7 +31,7 @@
                        <td>
                                 <div class="control-group">
 					       	      <div class="controls">
-						          	   <input type="text" style="width:50%;" class="input-xlarge" id="pattern1<?php echo $i; ?>" name="pattern1<?php echo $i; ?>" rel="popover" value="<?php echo $p->pattern1;?>" />
+						          	   <input type="text" style="width:50%;" class="input-xlarge" id="pattern1<?php echo $i;?>" name="pattern1<?php echo $i; ?>" rel="popover" value="<?php echo $p->pattern1;?>" />
                     
 			                         </div>
 		                          </div>
@@ -49,15 +50,19 @@
 			                         </div>
 		                          </div>
                         </td>
-						<td>
-						<div class="control-group">
+                                 <td>
+                                <div class="control-group">
 					       	         <div class="controls">
-						          	   <input type="text"  readonly="" style="width:50%;" class="input-xlarge" id="" name="" rel="popover" value="<?php echo $p->qcount;?>" />
+						          	   <input type="text"  readonly="" style="width:50%;" class="input-xlarge" id="question_count" name="" rel="popover" value="<?php echo $p->qcount;?>" />
 			                         </div>
 		                          </div>
-                        </td>    
-                      </tr>  
-                   <?php $i++; }  ?>
+                        </td>
+                        
+                        </tr>
+                   <?php $i++; }
+                        $j=$i-1;   
+                     ?>
+					 <input type="hidden" id="row_count" name="row_count" rel="popover" value="<?php echo $j; ?>" />
                    </table>
                    <div class="control-group">
 						<label class="control-label"></label>
@@ -72,3 +77,40 @@
                    </form>
                    </div>
                    </div>
+				   
+				   <script type="text/javascript">
+						var count=document.getElementById('row_count').value;
+						var row_count=document.getElementById('question_count').value;
+						for(var i=0;i<count;i++)
+						{
+							var j=i+1;
+							$('#pattern1'+j).keyup(function(){	
+							var pattern1= $('#pattern1'+i).val();
+								console.log(pattern1);
+								console.log(row_count);
+								if(pattern1>row_count)
+								{
+									console.log(pattern1);
+									console.log(row_count);
+									alert("Enter the value which is less than avaliable questions");
+									$('#pattern1'+i).val("").focus();	
+								}
+							}); 
+							$('#pattern2'+j).keyup(function(){
+							var pattern12= $('#pattern2'+i).val();
+								if(pattern12>row_count)
+								{
+									alert("Enter the value which is less than avaliable questions");
+									$('#pattern2'+i).val("").focus();	
+								}
+							}); 
+							$('#pattern3'+j).keyup(function(){
+							var pattern13= $('#pattern3'+i).val();
+								if(pattern13>row_count)
+								{
+									alert("Enter the value which is less than avaliable questions");
+									$('#pattern3'+i).val("").focus();	
+								}
+							}); 
+						}
+				   </script>
